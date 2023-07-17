96a9c4c99334bccad16bb62bb24075288092d8aa
Nataliachm <132951950+Nataliachm@users.noreply.github.com>
2023-07-17 17:53:20 -0500

    build: add initial project settings

---
+++ src/layout/Root/index.jsx
@@ -0,0 +1,9 @@
+// import { Outlet, useNavigation } from 'react-router-dom';
+import Header from '../../components/Header';
+
+function Root() {
+  return (
+    <Header />
+  );
+}
+export default Root;
