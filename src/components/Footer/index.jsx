c91f680e5c1e6a69e012ac7e69a356d47b982953
facamesi52 <facamesi52@gmail.com>
2023-07-19 13:46:41 -0500

    build: finish configurations

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,5 @@
-// eslint-disable-next-line react/function-component-definition
-const Footer = () => {
-  return (
-    <div>Este es un footer</div>
-  );
+const Index = () => {
+  return <div>index</div>;
 };
 
-export default Footer;
+export default Index;
