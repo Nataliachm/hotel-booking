c473bc9421614615bd506b4717f7a59a4fce8a5e
viamazoba <viamazoba@unal.edu.co>
2023-07-27 23:54:06 -0500

    fix: Figure out problem in carrousel

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 // import { RouterProvider } from 'react-router-dom';
 // import router from './router';
 import './index.css';
-import Header from './components/Header';
+// import Header from './components/Header';
 import MainHome from './components/MainHome';
 import Footer from './components/Footer';
 import './styles/base/base.scss';
 import './styles/base/reset.scss';
 import './styles/abstract/variables.scss';
 import './styles/abstract/mixins.scss';
-import FilterControl from './components/FilterControl';
+// import FilterControl from './components/FilterControl';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-      <Header />
-      <FilterControl />
+      {/* <Header />
+      <FilterControl /> */}
       <MainHome />
       <Footer />
     </>
