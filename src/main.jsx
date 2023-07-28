ace208a555b681e8fdf62f96b42cc16991785ecd
viamazoba <viamazoba@unal.edu.co>
2023-07-28 13:49:27 -0500

    feat: Creating FailPayment, NotFound and SuccessPayment components, also we did the router process

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { RouterProvider } from 'react-router-dom';
-// import router from './router';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 import './index.css';
-import Header from './components/Header';
-import MainHome from './components/MainHome';
-// import MainHotel from './components/MainHotel';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import MainHome from './components/MainHome';
+// // import MainHotel from './components/MainHotel';
+// import Footer from './components/Footer';
 import './styles/base/base.scss';
 import './styles/base/reset.scss';
 import './styles/abstract/variables.scss';
 import './styles/abstract/mixins.scss';
-import FilterControl from './components/FilterControl';
+// import FilterControl from './components/FilterControl';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <Header />
-      <FilterControl />
-      <MainHome />
-
-      <Footer />
-    </>
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NotFound.scss';
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <div className="container-notFound">
+        <div className="container-notFound__image">
+          <img src="https://web-sastre.com/wp-content/uploads/error-404-despues-migrar-restaurar-pagina-web-wordpress-01.png " alt="Traveller car" />
+        </div>
+        <div className="container-notFound__text">
+          <h2>Error 404: Page not found</h2>
+          <p>
+            The page you are looking for does not exist.
+            Please check the URL or navigate to another page.
+          </p>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default NotFound;
