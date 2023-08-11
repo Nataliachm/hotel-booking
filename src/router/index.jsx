c75886cc9d0b5da0edd00c21b358bf0927b8d0e8
NataliaChavarria <nataliachm97@gmail.com>
2023-08-11 18:09:44 -0500

    feat: creating authentication views

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,6 +6,11 @@ import SuccessPayment from '../pages/SuccessPayment';
 import FailPayment from '../pages/FailPayment';
 import NotFound from '../pages/NotFound';
 import TravellerInformation from '../pages/TravellerInformation';
+import Login from '../pages/Login';
+import Password from '../pages/Password';
+import AccountRecovery from '../pages/AccountRecovery';
+import RegisterPassword from '../pages/RegisterPassword';
+import Payment from '../pages/Payment';
 
 const router = createBrowserRouter([
   {
@@ -33,7 +38,27 @@ const router = createBrowserRouter([
         path: '/TravellerInformation',
         element: <TravellerInformation />,
       },
+      {
+        path: '/Payment',
+        element: <Payment />,
+      },
     ],
+  },
+  {
+    path: '/Login',
+    element: <Login />,
+  },
+  {
+    path: '/Login/Password',
+    element: <Password />,
+  },
+  {
+    path: '/Login/AccountRecovery',
+    element: <AccountRecovery />,
+  },
+  {
+    path: '/Login/RegisterPassword',
+    element: <RegisterPassword />,
   },
 ]);
 
