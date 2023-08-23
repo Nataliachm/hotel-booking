dd6fd78dc3ea86542498575365e3d3c55aff47ba
viamazoba <viamazoba@unal.edu.co>
2023-08-23 08:40:49 -0500

    fix: Deleting innecesary comments

--- src/pages/UserBookedRooms/infoRooms.js
+++ src/pages/UserBookedRooms/infoRooms.js
@@ -5,12 +5,12 @@ const rooms = [
     img: 'https://themes.pixelstrap.com/rica/assets/images/hotel/gallery/1.jpg',
     amenities: ['King/Twin', 'Shower', 'LCD TV', 'Couch'],
     inclusion: ['Wi-Fi', 'Breakfast', 'Non Refundable'],
-    reviews: 'Aquí deben ir las reviews del hotel o hacerlo en la tabla de hoteles mejor', // Clase hotel
-    labels: 'Recuerda colocar un array con los labels del hotel', // Clase hotel
+    reviews: 'Aquí deben ir las reviews del hotel o hacerlo en la tabla de hoteles mejor',
+    labels: 'Recuerda colocar un array con los labels del hotel',
     newPrice: 1000,
     previousPrice: 1250,
-    basicRoomPrice: 250, // Clase hotel
-    locationTags: 'Mina Road, Bur Dubai, Dubai, United Arab Emirates', // Clase hotel
+    basicRoomPrice: 250,
+    locationTags: 'Mina Road, Bur Dubai, Dubai, United Arab Emirates',
     country: 'United States',
     city: 'New York',
     hotelName: 'Paradise',
@@ -26,12 +26,12 @@ const rooms = [
     img: 'https://themes.pixelstrap.com/rica/assets/images/hotel/room/5.jpg',
     amenities: ['King/Twin', 'Shower', 'LCD TV', 'Pool View'],
     inclusion: ['Wi-Fi', 'Breakfast', 'Free Cancellation'],
-    reviews: 'Aquí deben ir las reviews del hotel o hacerlo en la tabla de hoteles mejor', // Clase hotel
-    labels: 'Recuerda colocar un array con los labels del hotel', // Clase hotel
+    reviews: 'Aquí deben ir las reviews del hotel o hacerlo en la tabla de hoteles mejor',
+    labels: 'Recuerda colocar un array con los labels del hotel',
     newPrice: 1100,
     previousPrice: 1350,
-    basicRoomPrice: 250, // Clase hotel
-    locationTags: 'Mina Road, Bur Dubai, Dubai, United Arab Emirates', // Clase hotel
+    basicRoomPrice: 250,
+    locationTags: 'Mina Road, Bur Dubai, Dubai, United Arab Emirates',
     country: 'Italy',
     city: 'Rome',
     hotelName: 'Landscape',
@@ -47,12 +47,12 @@ const rooms = [
     img: 'https://themes.pixelstrap.com/rica/assets/images/hotel/room/6.jpg',
     amenities: ['King/Twin', 'Pool View', 'LCD TV', 'Couch'],
     inclusion: ['Wi-Fi', 'Breakfast', 'Dinner & Lunch'],
-    reviews: 'Aquí deben ir las reviews del hotel o hacerlo en la tabla de hoteles mejor', // Clase hotel
-    labels: 'Recuerda colocar un array con los labels del hotel', // Clase hotel
+    reviews: 'Aquí deben ir las reviews del hotel o hacerlo en la tabla de hoteles mejor',
+    labels: 'Recuerda colocar un array con los labels del hotel',
     newPrice: 1800,
     previousPrice: 1950,
-    basicRoomPrice: 250, // Clase hotel
-    locationTags: 'Mina Road, Bur Dubai, Dubai, United Arab Emirates', // Clase hotel
+    basicRoomPrice: 250,
+    locationTags: 'Mina Road, Bur Dubai, Dubai, United Arab Emirates',
     country: 'Argentina',
     city: 'Buenos Aires',
     hotelName: 'Pampalare',
