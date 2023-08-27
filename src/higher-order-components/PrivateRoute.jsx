4a8f590503972f06f3fe5f53509715792a3ba4f3
viamazoba <viamazoba@unal.edu.co>
2023-08-26 22:22:44 -0500

    feat: Creating component of superior orden for routing pages

---
+++ src/higher-order-components/PrivateRoute.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable import/no-extraneous-dependencies */
+import { Navigate } from 'react-router-dom';
+import { useJwt } from 'react-jwt';
+
+import React from 'react';
+
+const PrivateRoute = ({ role = 'user', redirectRoute = '/', children }) => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return <Navigate to={redirectRoute} />;
+  }
+
+  if (role === 'user') {
+    // const token = localStorage.getItem('token');
+    const { isExpired } = useJwt(token);
+    const isAthorization = !isExpired;
+    return isAthorization ? children : <Navigate to={redirectRoute} />;
+  }
+
+  if (role === 'admin') {
+    // const token = localStorage.getItem('token');
+    const { decodedToken, isExpired } = useJwt(token);
+    // const roleAdmin = isExpired ? decodedToken.role : 'user';
+    const { role_name: roleName } = decodedToken;
+    const isAthorization = !isExpired;
+
+    return isAthorization ? (role === roleName ? children : <Navigate to={redirectRoute} />)
+      : <Navigate to={redirectRoute} />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
